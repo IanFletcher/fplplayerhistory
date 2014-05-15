bdb205fbb2f0698cfacbf3757b3d5e8c0176a90c
Ian Fletcher <ianfletcher9@gmail.com>
2014-05-15 15:23:06 +1000

    added new vender scope

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,6 +1,7 @@
 class GraphsController < ApplicationController
 	def top_10_bar
-		@data = Player.order(total_points: :desc).limit(10)
+		@data = Player.order(total_points: :desc).limit(10).to_json
+		logger.debug  Player.venue_split([214, 100]).inspect
 	#	PlayerHistory.find_by(player_id: @data.pluck[:id]).group(:venue).select(:total_points, :venue, :player_id).sum(:total_points)
 	end
 end
